6f6240d220c52ef5cac9bcca35dcb4816350165e
Lay Flags <code@layfla.gs>
2019-01-26 18:32:28 +0100

    use worker

--- src/index.js
+++ src/index.js
@@ -1,58 +1,42 @@
-import Parser from './parser';
-
-const isCtor = a => typeof a === 'string' && /^⟨.+⟩$/.test(a)
-
-const expandType = ([a, b, ...rest]) => {
-  if (!isCtor(a)) throw Error('`a` not a type string');
-  if (isCtor(b) && rest.length === 0) {
-    return [`${a}:${b}`];
-  }
-  if (Array.isArray(b) && isCtor(b[0]) && b.length <= 2 && rest.length === 0) {
-    return expandType([`${a}:${b[0]}`, ...b.slice(1)]);
-  }
-  return [a, b, ...rest];
-};
-
-const toFSA = action => {
-  const [type, ...payload] = expandType(action);
-  return payload.length > 0
-    ? { type: `${type}*`, payload: payload.length > 1 ? payload : payload[0] }
-    : { type };
-};
-
 const install = () => {
-  if (window.__REDUX_DEVTOOLS_EXTENSION__ && window.console) {
-    const devtools = window.__REDUX_DEVTOOLS_EXTENSION__.connect({
-      features: {
-        pause: true, // start/pause recording of dispatched actions
-        export: true, // export history of actions in a file
-      },
-    });
-    const consoleLog = window.console.log.bind(window.console);
-    window.console.log = (...args) => {
-      const match =
-        typeof args[0] === 'string' &&
-        args[0].match(/^\[Monitor:(init|update)\]: (.+)$/);
-      if (match) {
-        const [, type, content] = match;
-        switch (type) {
-          case 'init':
-            devtools.init(Parser.parse(content));
-            break;
-          case 'update':
-            {
-              const [action, state] = Parser.parse(content);
-              devtools.send(toFSA(action), state);
-            }
-            break;
-          default:
-          // will never happen
-        }
-      } else {
-        consoleLog(...args);
-      }
-    };
+  if (!window.__REDUX_DEVTOOLS_EXTENSION__) throw Error('Redux DevTools n/a');
+  if (!window.Worker) throw Error('Worker n/a');
+
+  const devtools = window.__REDUX_DEVTOOLS_EXTENSION__.connect({
+    features: {
+      pause: true, // start/pause recording of dispatched actions
+      export: true, // export history of actions in a file
+    },
+  });
+
+  const worker = new Worker('./worker.js');
+  worker.onmessage = event => {
+    const { type, payload } = event.data;
+    switch(type) {
+      case 'init':
+        devtools.init(payload);
+        break;
+      case 'update':
+        devtools.send(payload.action, payload.state);
+        break;
+      default:
+        // ignore
+    }
   }
-};
+
+  const consoleLog = console.log.bind(console);
+  console.log = (...args) => {
+    const match =
+      typeof args[0] === 'string' &&
+      args[0].match(/^\[Monitor:(init|update)\]: (.+)$/);
+
+    if (match) {
+      const [, type, content] = match;
+      worker.postMessage({ type, content });
+    } else {
+      consoleLog(...args);
+    }
+  }
+}
 
 export default install;
