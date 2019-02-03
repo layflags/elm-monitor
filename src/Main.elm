f93018915b22c2c49c19496f756dd53ac7832a76
ChristophP <deedop@hotmail.de>
2019-02-03 14:18:33 +0100

    patch parser deadEndsToString

--- src/Main.elm
+++ src/Main.elm
@@ -3,6 +3,7 @@ port module Main exposing (main)
 import Json.Encode as JE
 import OutputParser
 import Parser as P
+import ParserFix as PF
 
 
 port sendParsedData : JE.Value -> Cmd msg
@@ -29,20 +30,26 @@ init _ =
     ( (), Cmd.none )
 
 
+okJson : JE.Value -> JE.Value
+okJson value =
+    JE.object [ ( "Ok", value ) ]
+
+
+errJson : String -> JE.Value
+errJson str =
+    JE.object [ ( "Err", JE.string str ) ]
+
+
 update : Msg -> () -> ( (), Cmd Msg )
 update msg model =
     case msg of
         GotInput input ->
             case OutputParser.parse input of
                 Ok data ->
-                    ( (), sendParsedData data )
+                    ( (), sendParsedData <| okJson data )
 
                 Err err ->
-                    --let
-                    --_ =
-                    --Debug.log "DeadEnds" err
-                    --in
-                    ( (), Cmd.none )
+                    ( (), sendParsedData <| errJson <| PF.deadEndsToString err )
 
 
 subscriptions : () -> Sub Msg
